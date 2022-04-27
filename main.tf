f2f94b1103d4524c049a0c2249cd1f1d149e2ffa
tejavar <54866295+tejavar@users.noreply.github.com>
2022-04-27 15:26:53 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("<NAME>.json")
+
+  project = "hc-f4902757127142998e96816128a"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
